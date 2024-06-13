b3714d7e77ecbea7b038d32d5715ffa900451618
Sangeethasankar <sangeethasankar474@gmail.com>
2024-06-13 09:55:23 +0530

    update

--- src/router/productrouter.js
+++ src/router/productrouter.js
@@ -1,14 +1,15 @@
-// const express = require("express");
-// const router = express.Router();
-
-const ProductController = require("../Controller/productController");
-
-// router.route("/add/product").post(ProductController.createProductData);
-
-// module.exports = router;
 const express = require("express");
 const router = express.Router();
-// const productController = require("../controller/productController");
+const ProductController = require("../Controller/productController");
+const WishlistController = require("../Controller/wishlistController")
+
+// add product
+
 router.route("/add/product").post(ProductController.createProductData);
+
+
+// add wishlist
+
+router.route("/add/wishlist").post(WishlistController.addWishlist)
 
 module.exports = router;
